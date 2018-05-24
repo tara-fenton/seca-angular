2431044c3ca27257a7f50a7a3318a5aaa8807cc6
Tara Fenton <tarafenton@Taras-MBP-3.fios-router.home>
2018-05-23 23:40:46 -0400

    services add, same as before

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +9,7 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
@@ -21,10 +21,14 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   ngOnInit() {
-
+    this.getHeroes();
   }
 
 }
